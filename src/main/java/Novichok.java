1e68cfaa777f20a7e0bd316e3acbaefac3301049
deodatus <96296530+BogdanDobrynin@users.noreply.github.com>
2026-01-24 04:31:07 +0800

    Rebrand chatbot to Novichok and implemented skeletal version

---
+++ src/main/java/Novichok.java
@@ -0,0 +1,14 @@
+public class Novichok {
+    public static void main(String[] args) {
+        String logo = """
+             _   _            _      _             _
+            | \\ | | _____   _(_) ___| |__   ___   | | __
+            |  \\| |/ _ \\ \\ / / |/ __| '_ \\ / _ \\  | |/ /
+            | |\\  | (_) \\ V /| | (__| | | | (_) | |   <
+            |_| \\_|_\\___/ \\_/ |_|\\___|_| |_|\\___/ |_|\\_\\
+            """;
+        System.out.println(logo);
+        System.out.println("Greetings user!\n");
+        System.out.println("How can I serve you?");
+    }
+}
