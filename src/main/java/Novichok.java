537e6250f69d84af474366ebc57ae51535e43aa0
deodatus <96296530+BogdanDobrynin@users.noreply.github.com>
2026-01-29 15:48:11 +0800

    Add echo functionality and exit triggers

--- src/main/java/Novichok.java
+++ src/main/java/Novichok.java
@@ -1,14 +1,42 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Novichok {
     public static void main(String[] args) {
+        Scanner userScanner = new Scanner(System.in);
+
+        // user input exit keywords
+        List<String> exitKeywords = List.of("exit", "quit", "bye");
+
         String logo = """
-             _   _            _      _             _
-            | \\ | | _____   _(_) ___| |__   ___   | | __
-            |  \\| |/ _ \\ \\ / / |/ __| '_ \\ / _ \\  | |/ /
-            | |\\  | (_) \\ V /| | (__| | | | (_) | |   <
-            |_| \\_|_\\___/ \\_/ |_|\\___|_| |_|\\___/ |_|\\_\\
+             _   _           _      _            _
+            | \\ | | _____  _(_) ___| |__   ___  | | __
+            |  \\| |/ _\\ \\ / / |/ __| '_ \\ / _ \\ | |/ /
+            | |\\  | (_)\\ V /| | (__| | | | (_) ||   <
+            |_| \\_|\\___/\\_/ |_|\\___|_| |_|\\___/ |_|\\_\\
             """;
         System.out.println(logo);
         System.out.println("Greetings user!\n");
         System.out.println("How can I serve you?");
+        System.out.println("-----------------------\n");
+
+
+        while (true) {
+            // input acquisition and simple sanitization
+            String userCommand = userScanner.nextLine().trim();
+
+            // exit condition check
+            if (exitKeywords.contains(userCommand.toLowerCase())) {
+                System.out.println("\t-----------------------");
+                System.out.println("\tBye. Hope to see you again");
+                System.out.println("\t-----------------------");
+                break;
+            }
+
+            // command echo
+            System.out.println("\t-----------------------");
+            System.out.println("\t" + userCommand);
+            System.out.println("\t-----------------------\n");
+        }
     }
 }
